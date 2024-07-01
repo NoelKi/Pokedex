6a92c072d9bb15f44502092589ac12f3392db8b1
Kieran Mai <kieran.noel@icloud.com>
2024-07-01 15:13:35 +0200

    add script

--- src/script.js
+++ src/script.js
@@ -1,3 +1,31 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+pokemons = [];
+
+
 function init() {
-    
+    pokemonResponse = getAPokemon('1/species');
+}
+
+async function onloadFunc() {
+    let userResponse = await getAllUser("names");
+
+    let UserKeysArray = Object.keys(userResponse)
+
+    console.log(UserKeysArray);
+
+    await addEditSingleUser();
+}
+
+async function getAPokemon(path = "") {
+    try {
+        const response = await fetch(BASE_URL + path);
+        if (!response.ok) {
+            throw new Error('network does not answer correctly');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Fehler:', error);
+    }
 }
