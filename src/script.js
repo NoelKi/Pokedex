05631fc11dbf8b2e42ea28966419187e49afdb4c
Kieran Mai <kieran.noel@icloud.com>
2024-07-02 16:21:32 +0200

    add info-card script

--- src/script.js
+++ src/script.js
@@ -1,20 +1,97 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+const ALL_URL = "https://pokeapi.co/api/v2/pokemon"
+const EVO_URL = "https://pokeapi.co/api/v2/evolution-chain/"
 
-pokemons = [];
+let pokemons = [];
+let names = [];
 
-
-function init() {
-    pokemonResponse = getAPokemon('1/species');
+async function init() {
+    showLoadingSpinner();
+    await loadPokemons(100, 0);
+    console.log(pokemons);
+    renderPokemons();
+    // document.getElementById('search').addEventListener('keydown',function(e){filterNames()});
 }
 
-async function onloadFunc() {
-    let userResponse = await getAllUser("names");
+async function loadPokemons(amount = 1, offset = 0) {
+    for (let i = offset + 1; i < amount + 1; i++) {
+        let pokemonResponse = await getAPokemon(`${i}`);
+        pokemons.push(pokemonResponse);
+        names.push(pokemonResponse.name); 
+    }
+}
 
-    let UserKeysArray = Object.keys(userResponse)
+async function loadPokemonsFast(amount = 1, offset = 0) {
+    let pokemonResponse = await getAPokemon(`?limit=${amount}&offset=${offset}`);
+    console.log(pokemonResponse);
+    pokemons.push(pokemonResponse.results.map(pokemon => pokemon.name));
+    
+    names.push(pokemons.name); 
 
-    console.log(UserKeysArray);
+}
 
-    await addEditSingleUser();
+function renderPokemons() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+    for (let i = 0; i < pokemons.length; i++) {
+        pokemons[i].name = makeFirstLetterUpperCase(pokemons[i].name)
+        content.innerHTML += createCardHtml(i);
+    }
+}
+
+function renderInfoCard(index) {
+    const content = document.getElementById('info-content');
+    content.innerHTML = '';
+    const pokemon = pokemons[index];
+    content.innerHTML = createInfoHtml(pokemon);
+    document.body.style.overflow = "hidden";
+}
+
+function createInfoHtml(pokemon) {
+    return `
+    <div class="info-container" onclick="closeInfoCard()">
+        <div class="info-card" onclick="event.stopPropagation()">
+            <div class="info-card-top">
+                <span id="info-card-id">
+                    #${pokemon.id}
+                </span>
+                <span id="info-card-name">
+                    ${pokemon.name}
+                </span>
+            </div>
+            <div class="info-card-img ${pokemon.types[0].type.name}">
+                <img id="pokemon-img" src="${pokemon.sprites.front_default}">
+            </div>
+            <div class="info-card-bottom">
+                <div class="evolution">
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+function createCardHtml(i) {
+    pokemon = pokemons[i];
+    return `
+    <div class="card" onclick="renderInfoCard(${i})">
+        <div class="card-top">
+            <span id="pokemon_id">
+                #${pokemon.id}
+            </span>
+            <span id="pokemon-name">
+                ${pokemon.name}
+            </span>
+        </div>
+        <div class="card-img ${pokemon.types[0].type.name}">
+            <img id="pokemon-img" src="${pokemon.sprites.front_default}">
+        </div>
+        <div class="card-bottom">
+            <div>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
 async function getAPokemon(path = "") {
@@ -24,8 +101,80 @@ async function getAPokemon(path = "") {
             throw new Error('network does not answer correctly');
         }
         const data = await response.json();
-        console.log(data);
+        return data;
     } catch (error) {
         console.error('Fehler:', error);
     }
 }
+
+async function getPokemons(path = "") {
+    try {
+        const response = await fetch(BASE_URL + path);
+        if (!response.ok) {
+            throw new Error('network does not answer correctly');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fehler:', error);
+    }
+}
+
+
+async function getTheEvolution(path = "") {
+    try {
+        const response = await fetch(EVO_URL + path);
+        if (!response.ok) {
+            throw new Error('network does not answer correctly');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fehler:', error);
+    }
+}
+
+function makeFirstLetterUpperCase(string) {
+    string = string[0].toUpperCase() + string.slice(1);
+    return string
+}
+
+function makeCorrectId(id) {
+    if (id == 1) {
+        return id
+    } else {
+        id = ((id - 1) / 3)+ 1;
+        return id
+    }
+}
+
+function showLoadingSpinner() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+    content.innerHTML = `<div class='spinner'>
+        <img src='./img/icons/pokemon-logo.svg' alt='loading-spinner'>
+    </div>`;
+}
+
+function closeInfoCard() {
+    const infoContent = document.getElementById('info-content');
+    infoContent.innerHTML = '';
+    document.body.style.overflow = "visible";
+}
+
+function filterNames(event) {
+    console.log(event);
+    const search = event.target.value.toLowerCase();
+    const content = document.getElementById('content');
+    console.log(search);
+    content.innerHTML = '';
+
+    for (let index = 0; index < names.length; index++) {
+        let name = names[index];
+        name = name.toLowerCase();
+        if(name.includes(search)) {
+            content.innerHTML += createCardHtml(index);
+        }
+    }
+}
+
